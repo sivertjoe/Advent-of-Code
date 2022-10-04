30e45b6f727e6db42debb4d0b5e50f6970387f8f
Sivert Johansen <sivert-joh@hotmail.com>
2022-10-04 13:48:41 +0200

    show time in day 3

--- 2020/day_03/main.go
+++ 2020/day_03/main.go
@@ -1,72 +1,76 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "log"
-
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
 )
 
-func readFile() [] string {
-    f, err := os.Open("input")
+func readFile() []string {
+	f, err := os.Open("input")
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    defer f.Close()
+	defer f.Close()
 
-    data := []string{}
+	data := []string{}
 
-    scanner := bufio.NewScanner(f)
-    for scanner.Scan() {
-        data = append(data, scanner.Text())
-    }
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
-    
-    return data
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
+func timeFunc[T any, S any](f func(T) S, arg T) {
+	t0 := time.Now()
+	res := f(arg)
+	elapsed := time.Now().Sub(t0).Milliseconds()
+	fmt.Printf("(%dms)\t%v\n", elapsed, res)
 }
 
 func main() {
-    
-    data := readFile()
-    fmt.Println(partOne(data))
-    fmt.Println(partTwo(data))
+	data := readFile()
+    timeFunc(partOne, data)
+    timeFunc(partTwo, data)
 }
 
 func traverse(data []string, right int, down int) int {
-    posX := 0
-    posY := 0
-    counter := 0
-    
-    for posY < len(data) {
-        if data[posY][posX] == '#'{
-            counter += 1
-        }
-        
-        posX = (posX + right) % len(data[0])
-        posY += down
-    }
-    
-    return counter
+	posX := 0
+	posY := 0
+	counter := 0
+
+	for posY < len(data) {
+		if data[posY][posX] == '#' {
+			counter += 1
+		}
+
+		posX = (posX + right) % len(data[0])
+		posY += down
+	}
+
+	return counter
 }
 
-
 func partOne(data []string) int {
-    return traverse(data, 3, 1)
+	return traverse(data, 3, 1)
 }
 
 func partTwo(data []string) int {
-    vals := []int{1, 1, 3, 1, 5, 1, 7, 1, 1, 2}
-    sum := 1
-    for i := 0; i < len(vals); i += 2 { 
-        x := vals[i]
-        y := vals[i + 1]
-        sum *= traverse(data, x, y)        
-    }
-    return sum
+	vals := []int{1, 1, 3, 1, 5, 1, 7, 1, 1, 2}
+	sum := 1
+	for i := 0; i < len(vals); i += 2 {
+		x := vals[i]
+		y := vals[i+1]
+		sum *= traverse(data, x, y)
+	}
+	return sum
 }
