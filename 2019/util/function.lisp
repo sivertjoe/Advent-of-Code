e0eeffc95fef7d4e393055e298ef5bcca3cf9b00
Sivert Johansen <sivert-joh@hotmail.com>
2022-10-18 09:52:12 +0200

    add generic bfs to function

--- 2019/util/function.lisp
+++ 2019/util/function.lisp
@@ -5,3 +5,16 @@
         (funcall (first functions)
                  (apply (apply #'compose (rest functions))
                         args)))))
+(defun bfs (&key from end neighbors test)
+  (let ((visited (make-hash-table :test test))
+        (queue (make-queue :initial-contents (list (list from 0)))))
+    (setf (gethash from visited ) t)
+    (loop for next = (queue-pop queue)
+          while next
+          when (equal (car next) end)
+          return (cadr next)
+          do
+          (loop for neighbor in (funcall neighbors next)  do
+                (unless (gethash neighbor visited)
+                  (setf (gethash neighbor visited) t)
+                  (queue-push (list neighbor (1+ (cadr next))) queue))))))
