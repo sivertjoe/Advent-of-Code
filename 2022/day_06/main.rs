cbe58ee61546ff18e18153938a58dd60a8c2240a
Sivert Johansen <sivert-joh@hotmail.com>
2022-12-06 07:45:11 +0100

    add day 6

---
+++ 2022/day_06/main.rs
@@ -0,0 +1,67 @@
+use std::collections::*;
+
+fn solve(input: &[String], len: usize) -> usize
+{
+    input[0]
+        .as_bytes()
+        .windows(len)
+        .position(|w| w.iter().collect::<HashSet<_>>().len() == len)
+        .unwrap()
+        + len
+}
+fn task_one(input: &[String]) -> usize
+{
+    solve(input, 4)
+}
+
+fn task_two(input: &[String]) -> usize
+{
+    solve(input, 14)
+}
+
+fn main()
+{
+    let input = read_input(get_input_file());
+    time(Task::One, task_one, &input);
+    time(Task::Two, task_two, &input);
+}
+
+fn read_input<P>(path: P) -> Vec<String>
+where
+    P: AsRef<std::path::Path>,
+{
+    std::fs::read_to_string(path).unwrap().lines().map(String::from).collect()
+}
+
+enum Task
+{
+    One,
+    Two,
+}
+
+fn time<F, T, U>(task: Task, f: F, arg: T)
+where
+    F: Fn(T) -> U,
+    U: std::fmt::Display,
+{
+    let t = std::time::Instant::now();
+    let res = f(arg);
+    let elapsed = t.elapsed().as_millis();
+
+    match task
+    {
+        Task::One =>
+        {
+            println!("({}ms)\tTask one: \x1b[0;34;34m{}\x1b[0m", elapsed, res);
+        },
+        Task::Two =>
+        {
+            println!("({}ms)\tTask two: \x1b[0;33;10m{}\x1b[0m", elapsed, res);
+        },
+    };
+}
+
+fn get_input_file() -> String
+{
+    std::env::args().nth(1).unwrap_or_else(|| "input".to_string())
+}
