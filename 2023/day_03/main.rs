f0713114be240f7c5afe5b8a6148eb4954c91f0e
sivertjoe <sivert-joh@hotmail.com>
2023-12-03 08:35:18 +0100

    add: day3 💩

---
+++ 2023/day_03/main.rs
@@ -0,0 +1,207 @@
+use std::collections::*;
+
+#[derive(Debug)]
+enum Cell {
+    Gear(usize),
+    Sym(char),
+}
+
+#[derive(Debug)]
+struct Elem {
+    cell: Cell,
+    x: isize,
+    y: isize,
+}
+
+fn task_one(input: &[String]) -> usize {
+    let mut elems = Vec::new();
+    let mx = input[0].as_bytes().len() - 1;
+    for (y, line) in input.iter().enumerate() {
+        let y = y as isize;
+        let mut flag = false;
+        let mut acc = String::new();
+        for (x, ch) in line.bytes().enumerate() {
+            if !flag && ch.is_ascii_digit() {
+                flag = true;
+                acc.clear();
+            }
+
+            if flag && !ch.is_ascii_digit() {
+                flag = false;
+                let num = acc.parse::<usize>().unwrap();
+                elems.push(Elem {
+                    cell: Cell::Gear(num),
+                    x: x as isize - 1,
+                    y,
+                });
+            }
+
+            if flag && ch.is_ascii_digit() {
+                acc.push(ch as char);
+            }
+
+            if x == mx && flag {
+                flag = false;
+                let num = acc.parse::<usize>().unwrap();
+                elems.push(Elem {
+                    cell: Cell::Gear(num),
+                    x: x as isize,
+                    y,
+                });
+            }
+
+            if ch != b'.' && !ch.is_ascii_digit() {
+                elems.push(Elem {
+                    cell: Cell::Sym(ch as char),
+                    x: x as isize,
+                    y,
+                });
+            }
+        }
+    }
+
+    let mut sum = 0;
+    for elem in elems.iter() {
+        if matches!(elem.cell, Cell::Sym(a)) {
+            let num = find(elem, &elems);
+            sum += num.into_iter().sum::<usize>();
+        }
+    }
+
+    sum
+}
+
+fn find(elem: &Elem, elems: &[Elem]) -> Vec<usize> {
+    let mut sum = Vec::new();
+    for e in elems {
+        if let Cell::Gear(g) = e.cell {
+            let len = g.to_string().len();
+            let x0 = e.x - (len as isize - 1);
+            let x1 = e.x;
+
+            if (x0..=x1)
+                .find(|x| e.y.abs_diff(elem.y) <= 1 && x.abs_diff(elem.x) <= 1)
+                .is_some()
+            {
+                sum.push(g)
+            }
+        }
+    }
+    sum
+}
+
+fn task_two(input: &[String]) -> usize {
+    let mut elems = Vec::new();
+    let mx = input[0].as_bytes().len() - 1;
+    for (y, line) in input.iter().enumerate() {
+        let y = y as isize;
+        let mut flag = false;
+        let mut acc = String::new();
+        for (x, ch) in line.bytes().enumerate() {
+            if !flag && ch.is_ascii_digit() {
+                flag = true;
+                acc.clear();
+            }
+
+            if flag && !ch.is_ascii_digit() {
+                flag = false;
+                let num = acc.parse::<usize>().unwrap();
+                elems.push(Elem {
+                    cell: Cell::Gear(num),
+                    x: x as isize - 1,
+                    y,
+                });
+            }
+
+            if flag && ch.is_ascii_digit() {
+                acc.push(ch as char);
+            }
+
+            if x == mx && flag {
+                flag = false;
+                let num = acc.parse::<usize>().unwrap();
+                elems.push(Elem {
+                    cell: Cell::Gear(num),
+                    x: x as isize,
+                    y,
+                });
+            }
+
+            if ch != b'.' && !ch.is_ascii_digit() {
+                elems.push(Elem {
+                    cell: Cell::Sym(ch as char),
+                    x: x as isize,
+                    y,
+                });
+            }
+        }
+    }
+
+    let mut sum = 0;
+    for elem in elems.iter() {
+        if let Cell::Sym(a) = elem.cell {
+            let num = find(elem, &elems);
+            if a == '*' && num.len() == 2 {
+                sum += num[0] * num[1];
+            }
+        }
+    }
+
+    sum
+}
+
+fn main() {
+    let input = read_input(get_input_file());
+    time(Task::One, task_one, &input);
+    time(Task::Two, task_two, &input);
+}
+
+fn read_input<P>(path: P) -> Vec<String>
+where
+    P: AsRef<std::path::Path>,
+{
+    std::fs::read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect()
+}
+
+enum Task {
+    One,
+    Two,
+}
+
+fn time<F, T, U>(task: Task, f: F, arg: T)
+where
+    F: Fn(T) -> U,
+    U: std::fmt::Display,
+{
+    let t = std::time::Instant::now();
+    let res = f(arg);
+    let elapsed = t.elapsed();
+    let fmt = std::env::var("TASKUNIT").unwrap_or("ms".to_owned());
+
+    let (u, elapsed) = match fmt.as_str() {
+        "ms" => ("ms", elapsed.as_millis()),
+        "ns" => ("ns", elapsed.as_nanos()),
+        "us" => ("μs", elapsed.as_micros()),
+        "s" => ("s", elapsed.as_secs() as u128),
+        _ => panic!("unsupported time format"),
+    };
+
+    match task {
+        Task::One => {
+            println!("({}{u})\tTask one: \x1b[0;34;34m{}\x1b[0m", elapsed, res);
+        }
+        Task::Two => {
+            println!("({}{u})\tTask two: \x1b[0;33;10m{}\x1b[0m", elapsed, res);
+        }
+    };
+}
+
+fn get_input_file() -> String {
+    std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "input".to_string())
+}
