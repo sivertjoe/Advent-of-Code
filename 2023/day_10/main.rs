b1726a5e7721d539ef7b8c7d9381c6bf7f2f91a1
sivertjoe <sivert-joh@hotmail.com>
2023-12-10 20:21:42 +0100

    update(day10): better performance

--- 2023/day_10/main.rs
+++ 2023/day_10/main.rs
@@ -1,4 +1,4 @@
-use std::collections::*;
+type HashSet<K> = std::collections::HashSet<K, fxhash::FxBuildHasher>;
 
 fn parse(input: &[String]) -> Vec<Vec<u8>> {
     input.iter().map(|line| line.as_bytes().to_vec()).collect()
@@ -15,7 +15,7 @@ fn start(vec: &[Vec<u8>]) -> (usize, usize) {
     unreachable!()
 }
 
-fn figure_out_start(vec: &[Vec<u8>], pos: (usize, usize)) -> u8 {
+fn get_start_character(vec: &[Vec<u8>], pos: (usize, usize)) -> u8 {
     let my = vec.len() as isize;
     let mx = vec[0].len() as isize;
     let x = pos.0 as isize;
@@ -134,171 +134,76 @@ fn neighbors(vec: &[Vec<u8>], pos: (usize, usize)) -> Vec<(usize, usize)> {
     ns
 }
 
-fn solve(vec: &[Vec<u8>], start: (usize, usize)) -> HashMap<(usize, usize), usize> {
-    let mut dist = HashMap::new();
-    dist.insert(start, 0);
+fn get_map(vec: &[Vec<u8>], start: (usize, usize)) -> HashSet<(usize, usize)> {
+    let mut seen = HashSet::default();
+    let mut stack = vec![start];
 
-    let mut heap = BinaryHeap::new();
-
-    heap.push((0, start));
-
-    while let Some((cost, pos)) = heap.pop() {
-        if cost > dist[&pos] {
-            continue;
-        }
-
+    while let Some(pos) = stack.pop() {
         for pair in neighbors(vec, pos) {
-            let temp = dist.entry(pair).or_insert(usize::MAX);
-            if cost + 1 < *temp {
-                *temp = cost + 1;
-                heap.push((cost + 1, pair));
+            if seen.insert(pair) {
+                stack.push(pair);
             }
         }
     }
-    // dist.into_values().max().unwrap()
-    dist
+
+    seen
+}
+
+fn is_point_inside(vec: &[Vec<u8>], pos: (usize, usize), map: &HashSet<(usize, usize)>) -> bool {
+    let mut count = 0;
+    let mut open_down = false;
+    let mut open_up = false;
+
+    for x in pos.0 + 1..vec[0].len() {
+        if !map.contains(&(x, pos.1)) {
+            continue;
+        }
+
+        match vec[pos.1][x] {
+            b'|' => count += 1,
+            b'F' => open_down = true,
+            b'L' => open_up = true,
+            b'7' => {
+                count += if open_down { 2 } else { 1 };
+                open_up = false;
+                open_down = false;
+            }
+            b'J' => {
+                count += if open_up { 2 } else { 1 };
+                open_up = false;
+                open_down = false;
+            }
+            _ => {}
+        }
+    }
+
+    count % 2 == 1
 }
 
 fn task_one(input: &[String]) -> usize {
     let mut vec = parse(input);
     let start = start(&vec);
-    let ch = figure_out_start(&vec, start);
+    let ch = get_start_character(&vec, start);
     vec[start.1][start.0] = ch;
-    solve(&vec, start).into_values().max().unwrap()
-}
-
-fn expand_char(
-    center: (usize, usize),
-    xy: (usize, usize),
-    ch: u8,
-    new: &mut [Vec<u8>],
-    other: &[Vec<u8>],
-) {
-    match ch {
-        b'.' => {
-            new[center.1][center.0] = b'.';
-        }
-        b'S' => {
-            let ch = figure_out_start(&other, xy);
-            expand_char(center, xy, ch, new, other);
-            new[center.1][center.0] = b'S'; // center
-        }
-        b'-' => {
-            new[center.1][center.0 - 1] = b'-';
-            new[center.1][center.0] = b'-';
-            new[center.1][center.0 + 1] = b'-';
-        }
-        b'|' => {
-            new[center.1 - 1][center.0] = b'|';
-            new[center.1][center.0] = b'|';
-            new[center.1 + 1][center.0] = b'|';
-        }
-        b'F' => {
-            new[center.1][center.0 + 1] = b'-';
-            new[center.1][center.0] = b'F';
-            new[center.1 + 1][center.0] = b'|';
-        }
-        b'7' => {
-            new[center.1][center.0 - 1] = b'-';
-            new[center.1][center.0] = b'7';
-            new[center.1 + 1][center.0] = b'|';
-        }
-        b'L' => {
-            new[center.1][center.0 + 1] = b'-';
-            new[center.1][center.0] = b'L';
-            new[center.1 - 1][center.0] = b'|';
-        }
-        b'J' => {
-            new[center.1][center.0 - 1] = b'-';
-            new[center.1][center.0] = b'J';
-            new[center.1 - 1][center.0] = b'|';
-        }
-
-        _ => unreachable!(),
-    }
-}
-
-fn expand(vec: &[Vec<u8>]) -> Vec<Vec<u8>> {
-    let mut new = vec![vec![b'x'; 3 * vec[0].len()]; 3 * vec.len()];
-
-    for y in 0..vec.len() {
-        for x in 0..vec[0].len() {
-            let elem = vec[y][x];
-            let center = (3 * x + 1, 3 * y + 1);
-            expand_char(center, (x, y), elem, &mut new, &vec);
-        }
-    }
-
-    new
-}
-
-fn _find_start(new: &[Vec<u8>]) -> (usize, usize) {
-    for y in 0..new.len() {
-        for x in 0..new[0].len() {
-            if new[y][x] == b'S' {
-                return (x, y);
-            }
-        }
-    }
-    unreachable!()
-}
-
-fn find_point(new: &[Vec<u8>], dist: &HashMap<(usize, usize), usize>) -> (usize, usize) {
-    for y in 0..new.len() {
-        for x in 0..new[0].len() - 1 {
-            let elem = new[y][x + 1];
-
-            if new[y][x] == b'|' && dist.contains_key(&(x, y)) && (elem == b'.' || elem == b'x') {
-                return (x + 1, y);
-            }
-        }
-    }
-    unreachable!()
+    get_map(&vec, start).len() / 2
 }
 
 fn task_two(input: &[String]) -> usize {
-    let vec = parse(input);
-
-    let mut new = expand(&vec);
-    let astart = _find_start(&new);
-
+    let mut vec = parse(input);
     let start = start(&vec);
-    let ch = figure_out_start(&vec, start);
+    let ch = get_start_character(&vec, start);
+    vec[start.1][start.0] = ch;
+    let map = get_map(&vec, start);
 
-    new[astart.1][astart.0] = ch;
-    let dist = solve(&new, astart);
-
-    let mut mapping = HashMap::new();
-    let start = find_point(&new, &dist);
-    mapping.insert(start, new[start.1][start.0]);
-    let mut stack = vec![start];
-
-    while let Some(p) = stack.pop() {
-        for p in [
-            (p.0 + 1, p.1),
-            (p.0 - 1, p.1),
-            (p.0, p.1 + 1),
-            (p.0, p.1 - 1),
-        ] {
-            let elem = new[p.1][p.0];
-            if !dist.contains_key(&p) && mapping.insert(p, elem).is_none() {
-                stack.push(p);
+    let mut count = 0;
+    for y in 0..vec.len() {
+        for x in 0..vec[0].len() {
+            if !map.contains(&(x, y)) && is_point_inside(&vec, (x, y), &map) {
+                count += 1;
             }
         }
     }
-
-    let mut dot = 0;
-    let mut sym = 0;
-    for elem in mapping.into_values().filter(|elem| *elem != b'x') {
-        match elem {
-            b'.' => dot += 1,
-            _ if [b'|', b'-', b'7', b'F', b'J', b'L'].contains(&elem) => {
-                sym += 1;
-            }
-            _ => {}
-        }
-    }
-    dot + (sym / 3)
+    count
 }
 
 fn main() {
