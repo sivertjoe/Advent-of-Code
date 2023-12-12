9b3220ed23f5f55ee13713990b1699cf6dd1b68d
sivertjoe <sivert-joh@hotmail.com>
2023-12-12 07:22:32 +0100

    add(day12):  💩

--- 2023/day_12/main.rs
+++ 2023/day_12/main.rs
@@ -1,96 +1,88 @@
 use std::collections::*;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum Record {
-    Operational,
-    Damaged,
-    Unknown,
-}
-
-fn verify(curr: Vec<Record>, nums: &[usize]) -> bool {
-    let mut i = 0;
-    for num in nums {
-        while i < curr.len() {
-            if curr[i] == Record::Damaged {
-                break;
+fn _solve2(
+    recs_i: usize,
+    recs: &mut [u8],
+    nums_i: usize,
+    nums: &[usize],
+    prev_hash: usize,
+    //dp: &mut HashMap<(recs_i, nums_i, prev_hash), usize>
+    dp: &mut HashMap<(usize, usize, usize, u8), usize>
+) -> usize{
+    let mut loc = 0;
+    if recs_i >= recs.len() {
+        if prev_hash > 0 {
+            if nums_i == nums.len() - 1 && prev_hash == nums[nums_i] {
+                return 1;
             }
-            i += 1;
-        }
-        let dcount = curr[i..]
-            .iter()
-            .take_while(|rec| **rec == Record::Damaged)
-            .count();
+        } else { 
+            if nums_i >= nums.len()  {
+                return 1;
+            } else {
 
-        if i + num > curr.len() {
-            return false;
+            }
         }
-
-        if !curr[i..i + num].iter().all(|rec| *rec == Record::Damaged) || *num != dcount {
-            return false;
-        }
-        i += num;
+        //return true;
+        return 0;
     }
-    let s1 = curr.iter().filter(|rec| **rec == Record::Damaged).count();
-    let s2 = nums.iter().copied().sum();
+    if nums_i >= nums.len() {
+        if recs[recs_i..].iter().any(|r| *r == b'#') {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 
-    s1 == s2
+    if prev_hash > 0 && prev_hash > nums[nums_i] {
+        return 0;
+    }
+    let elem = recs[recs_i];
+    match elem {
+        b'?' => {
+            recs[recs_i] = b'#';
+
+            let k1 = (recs_i, nums_i, prev_hash, b'#');
+            let k2 = (recs_i, nums_i, prev_hash, b'.');
+
+            if !dp.contains_key(&k1)
+            {
+                let r = _solve2(recs_i, recs, nums_i, nums, prev_hash, dp);
+                dp.insert(k1, r);
+            }
+            loc += *dp.get(&k1).unwrap();
+
+            recs[recs_i] = b'.';
+            if !dp.contains_key(&k2)
+            {
+                let r = _solve2(recs_i, recs, nums_i, nums, prev_hash, dp);
+                dp.insert(k2, r);
+            }
+            loc += *dp.get(&k2).unwrap();
+
+            recs[recs_i] = b'?';
+        }
+        b'#' => {
+            loc += _solve2(recs_i + 1, recs, nums_i, nums, prev_hash + 1, dp);
+        }
+        b'.' => {
+            if prev_hash > 0 && prev_hash == nums[nums_i] {
+                loc += _solve2(recs_i + 1, recs, nums_i + 1, nums, 0, dp);
+            } else if prev_hash == 0 {
+                loc += _solve2(recs_i + 1, recs, nums_i, nums, 0, dp);
+            }
+        }
+        _ => unreachable!()
+    }
+    loc
 }
 
-fn _print(recs: &[Record]) {
-    let mut builder = String::new();
-    for rec in recs {
-        let cha = match rec {
-            Record::Unknown => '?',
-            Record::Damaged => '#',
-            _ => '.',
-        };
-        builder.push(cha);
-    }
-    println!("{}", builder);
-}
-
-fn _solve(i: usize, recs: &[Record], nums: &[usize], curr: Vec<Record>, count: &mut usize) {
-    if i >= recs.len() {
-        if verify(curr.clone(), nums) {
-            *count += 1;
-        }
-        return;
-    }
-    let mut curr = curr;
-
-    let elem = recs[i];
-    if elem == Record::Unknown {
-        let mut curr2 = curr.clone();
-        curr2.push(Record::Operational);
-        _solve(i + 1, recs, nums, curr2, count);
-
-        curr.push(Record::Damaged);
-        _solve(i + 1, recs, nums, curr, count);
-    } else {
-        curr.push(elem);
-        _solve(i + 1, recs, nums, curr, count);
-    }
-}
-
-/*fn solve(recs: &[Record], nums: &[usize]) -> usize {
-    let mut vecs = Vec::new();
-    _solve(&recs, &nums, &mut vecs);
-    0
-}*/
-
-fn parse(input: &[String]) -> Vec<(Vec<Record>, Vec<usize>)> {
+fn parse(input: &[String]) -> Vec<(Vec<u8>, Vec<usize>)> {
     input
         .iter()
         .map(|line| {
             let (fst, snd) = line.split_once(' ').unwrap();
             let fst = fst
                 .bytes()
-                .map(|b| match b {
-                    b'#' => Record::Damaged,
-                    b'.' => Record::Operational,
-                    b'?' => Record::Unknown,
-                    _ => unreachable!(),
-                })
                 .collect::<Vec<_>>();
             let snd = snd
                 .split(',')
@@ -106,20 +98,37 @@ fn task_one(input: &[String]) -> usize {
     let data = parse(input);
     let mut count = 0;
 
-    //_solve(0, &data[5].0, &data[5].1, vec![], &mut count);
-
     for (recs, nums) in data {
-        let mut local = 0;
-        _solve(0, &recs, &nums, vec![], &mut local);
-        _print(&recs);
-        println!("{}\n", local);
+        let local = _solve2(0, &mut recs.clone(), 0, &nums, 0, &mut HashMap::new());
         count += local;
     }
     count
 }
 
+fn transform((recs, nums): (Vec<u8>, Vec<usize>)) -> (Vec<u8>, Vec<usize>) {
+    let mut nrecs = Vec::new();
+    let mut nnums = Vec::new();
+
+    for _ in 0..5 {
+        nrecs.extend(recs.clone());
+        nrecs.push(b'?');
+        nnums.extend(nums.clone());
+    }
+    let _ = nrecs.pop();
+
+    (nrecs, nnums)
+}
+
 fn task_two(input: &[String]) -> usize {
-    unimplemented!()
+    let data = parse(input).into_iter().map(transform).collect::<Vec<_>>();
+    let mut count = 0;
+
+
+   for (recs, nums) in data {
+        let local = _solve2(0, &mut recs.clone(), 0, &nums,  0, &mut HashMap::new());
+        count += local;
+    }
+    count
 }
 
 fn main() {
