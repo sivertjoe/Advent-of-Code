d4067e14e151903cb49d6184f897eed28b8ec795
Sivert Johansen <sivert@hotmail.com>
2023-12-13 15:27:48 +0100

    update(day12) ✨

--- 2023/day_12/main.rs
+++ 2023/day_12/main.rs
@@ -1,89 +1,9 @@
-use std::collections::*;
-
-fn _solve2(
-    recs_i: usize,
-    recs: &mut [u8],
-    nums_i: usize,
-    nums: &[usize],
-    prev_hash: usize,
-    //dp: &mut HashMap<(recs_i, nums_i, prev_hash), usize>
-    dp: &mut HashMap<(usize, usize, usize, u8), usize>
-) -> usize{
-    let mut loc = 0;
-    if recs_i >= recs.len() {
-        if prev_hash > 0 {
-            if nums_i == nums.len() - 1 && prev_hash == nums[nums_i] {
-                return 1;
-            }
-        } else { 
-            if nums_i >= nums.len()  {
-                return 1;
-            } else {
-
-            }
-        }
-        //return true;
-        return 0;
-    }
-    if nums_i >= nums.len() {
-        if recs[recs_i..].iter().any(|r| *r == b'#') {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
-
-    if prev_hash > 0 && prev_hash > nums[nums_i] {
-        return 0;
-    }
-    let elem = recs[recs_i];
-    match elem {
-        b'?' => {
-            recs[recs_i] = b'#';
-
-            let k1 = (recs_i, nums_i, prev_hash, b'#');
-            let k2 = (recs_i, nums_i, prev_hash, b'.');
-
-            if !dp.contains_key(&k1)
-            {
-                let r = _solve2(recs_i, recs, nums_i, nums, prev_hash, dp);
-                dp.insert(k1, r);
-            }
-            loc += *dp.get(&k1).unwrap();
-
-            recs[recs_i] = b'.';
-            if !dp.contains_key(&k2)
-            {
-                let r = _solve2(recs_i, recs, nums_i, nums, prev_hash, dp);
-                dp.insert(k2, r);
-            }
-            loc += *dp.get(&k2).unwrap();
-
-            recs[recs_i] = b'?';
-        }
-        b'#' => {
-            loc += _solve2(recs_i + 1, recs, nums_i, nums, prev_hash + 1, dp);
-        }
-        b'.' => {
-            if prev_hash > 0 && prev_hash == nums[nums_i] {
-                loc += _solve2(recs_i + 1, recs, nums_i + 1, nums, 0, dp);
-            } else if prev_hash == 0 {
-                loc += _solve2(recs_i + 1, recs, nums_i, nums, 0, dp);
-            }
-        }
-        _ => unreachable!()
-    }
-    loc
-}
-
 fn parse(input: &[String]) -> Vec<(Vec<u8>, Vec<usize>)> {
     input
         .iter()
         .map(|line| {
             let (fst, snd) = line.split_once(' ').unwrap();
-            let fst = fst
-                .bytes()
-                .collect::<Vec<_>>();
+            let fst = fst.bytes().collect::<Vec<_>>();
             let snd = snd
                 .split(',')
                 .map(|d| d.parse::<usize>().unwrap())
@@ -94,20 +14,45 @@ fn parse(input: &[String]) -> Vec<(Vec<u8>, Vec<usize>)> {
         .collect()
 }
 
-fn task_one(input: &[String]) -> usize {
-    let data = parse(input);
-    let mut count = 0;
-
-    for (recs, nums) in data {
-        let local = _solve2(0, &mut recs.clone(), 0, &nums, 0, &mut HashMap::new());
-        count += local;
+fn arrangements((spring, nums): (Vec<u8>, Vec<usize>)) -> usize {
+    let mut spring = spring;
+    if let Some(idx) = spring.iter().rev().position(|elm| *elm != b'.') {
+        spring.truncate(spring.len() - idx + 1)
     }
-    count
+    spring.insert(0, b'.');
+
+    let mut dp = vec![0; spring.len() + 1];
+
+    for i in (0..spring.len()).take_while(|i| spring[*i] != b'#') {
+        dp[i] = 1;
+    }
+
+    for num in nums {
+        let mut count = 0;
+        let mut new = vec![0; spring.len()+1];
+
+        for (i, ch) in spring.iter().copied().enumerate() {
+            if ch == b'.' {
+                count = 0;
+            } else {
+                count += 1;
+            }
+            if ch != b'#' {
+                new[i + 1] += new[i];
+            }
+            if count >= num && spring[i - num] != b'#' {
+                new[i + 1] += dp[i - num];
+            }
+        }
+        dp = new;
+    }
+
+    *dp.last().unwrap()
 }
 
 fn transform((recs, nums): (Vec<u8>, Vec<usize>)) -> (Vec<u8>, Vec<usize>) {
-    let mut nrecs = Vec::new();
-    let mut nnums = Vec::new();
+    let mut nrecs = Vec::with_capacity(recs.len() * 5);
+    let mut nnums = Vec::with_capacity(nums.len() * 5);
 
     for _ in 0..5 {
         nrecs.extend(recs.clone());
@@ -119,16 +64,21 @@ fn transform((recs, nums): (Vec<u8>, Vec<usize>)) -> (Vec<u8>, Vec<usize>) {
     (nrecs, nnums)
 }
 
+type T = (Vec<u8>, Vec<usize>);
+fn solve<F: Fn(T) -> T>(input: &[String], f: F) -> usize {
+    parse(input)
+        .into_iter()
+        .map(f)
+        .map(arrangements)
+        .sum()
+}
+fn task_one(input: &[String]) -> usize {
+    use std::convert::identity;
+    solve(input, identity)
+}
+
 fn task_two(input: &[String]) -> usize {
-    let data = parse(input).into_iter().map(transform).collect::<Vec<_>>();
-    let mut count = 0;
-
-
-   for (recs, nums) in data {
-        let local = _solve2(0, &mut recs.clone(), 0, &nums,  0, &mut HashMap::new());
-        count += local;
-    }
-    count
+    solve(input, transform)
 }
 
 fn main() {
