87c36e3046c8463d0d461163fb9775f7089440a9
sivertjoe <sivert-joh@hotmail.com>
2023-12-17 14:27:43 +0100

    update(day17) ✨

--- 2023/day_17/main.rs
+++ 2023/day_17/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Reverse;
 use std::collections::*;
 
 fn parse(input: &[String]) -> Vec<Vec<usize>> {
@@ -11,52 +12,7 @@ fn parse(input: &[String]) -> Vec<Vec<usize>> {
         .collect()
 }
 
-fn neighbors(
-    vec: &[Vec<usize>],
-    idx: (usize, usize),
-    (gdir, count): (Dir, usize),
-) -> impl Iterator<Item = ((usize, usize), (Dir, usize))> {
-    let my = vec.len() as isize;
-    let mx = vec[0].len() as isize;
-    let y = idx.0 as isize;
-    let x = idx.1 as isize;
-    [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)]
-        .into_iter()
-        .filter(move |(y, x)| *x >= 0 && *x < mx && *y >= 0 && *y < my)
-        .map(|(y, x)| (y as usize, x as usize))
-        .map(move |(y, x)| {
-            let dir = if y == idx.0 {
-                if x > idx.1 {
-                    Dir::Right
-                } else {
-                    Dir::Left
-                }
-            } else if x == idx.1 {
-                if y > idx.0 {
-                    Dir::Down
-                } else {
-                    Dir::Up
-                }
-            } else {
-                unreachable!();
-            };
-
-            (y, x, dir)
-        })
-        .filter(move |(y, x, dir)| {
-            !((*dir == Dir::Up && gdir == Dir::Down)
-                || (*dir == Dir::Down && gdir == Dir::Up)
-                || (*dir == Dir::Left && gdir == Dir::Right)
-                || (*dir == Dir::Right && gdir == Dir::Left)
-                || (*dir == gdir && count == 2))
-        })
-        .map(move |(y, x, dir)| {
-            let ncount = if dir == gdir { count + 1 } else { 0 };
-            ((y, x), (dir, ncount))
-        })
-}
-
-#[derive(Hash, Eq, PartialEq, Ord, PartialOrd, Debug, Clone, Copy)]
+#[derive(Hash, Eq, PartialEq, Ord, PartialOrd, Clone, Copy)]
 enum Dir {
     Right,
     Left,
@@ -64,113 +20,7 @@ enum Dir {
     Down,
 }
 
-fn _print(vec: &[Vec<usize>], me: (usize, usize), dir: Dir) {
-    let mech = match dir {
-        Dir::Right => '>',
-        Dir::Left => '<',
-        Dir::Up => '^',
-        Dir::Down => 'v',
-    };
-    for y in 0..vec.len() {
-        for x in 0..vec[0].len() {
-            if (y, x) == me {
-                print!("{}", mech);
-            } else {
-                print!("{}", vec[y][x]);
-            }
-        }
-        println!();
-    }
-    println!();
-}
-
-fn _print2(
-    vec: &[Vec<usize>],
-    prev: &HashMap<((usize, usize), (Dir, usize)), ((usize, usize), (Dir, usize))>,
-    end: ((usize, usize), (Dir, usize)),
-) {
-    let mut list = Vec::new();
-    let mut next = end;
-    while let Some(elm) = prev.get(&next) {
-        assert!(elm.1 .1 < 3);
-        list.push(elm.clone());
-        next = elm.clone();
-    }
-
-    for y in 0..vec.len() {
-        for x in 0..vec[0].len() {
-            if let Some(e) = list.iter().find(|e| y == e.0 .0 && x == e.0 .1) {
-                let ch = match e.1 .0 {
-                    Dir::Right => '>',
-                    Dir::Left => '<',
-                    Dir::Up => '^',
-                    Dir::Down => 'v',
-                };
-                //print!("|{}{}|", ch, e.1 .1);
-                print!("{}", ch);
-            } else {
-                print!("{}", vec[y][x]);
-            }
-        }
-        println!();
-    }
-    println!();
-}
-
-fn _pause() {
-    use std::io::BufRead;
-    let _ = std::io::stdin().lock().read_line(&mut String::new());
-}
-
-fn task_one(input: &[String]) -> usize {
-    let vec = parse(input);
-
-    let mut seen = HashSet::new();
-
-    let start = (0, 0);
-
-    seen.insert((start, (Dir::Right, 0)));
-    //seen.insert((start, 0));
-
-    let mut stack = BinaryHeap::new();
-    use std::cmp::Reverse;
-
-    stack.push((Reverse(0), start, (Dir::Right, 0)));
-    stack.push((Reverse(0), start, (Dir::Down, 0)));
-
-    let end = (vec.len() - 1, vec[0].len() - 1);
-
-    let mut prev = HashMap::new();
-
-    // 1012
-    // 1008??
-    while let Some(_xd @ (cost, p, info)) = stack.pop() {
-        //println!("begin");
-        if p == end {
-            //_print2(&vec, &prev, (p, info));
-
-            return cost.0;
-        }
-        //println!("{:?} {}", stack.len(), cost.0);
-        // _print(&vec, p, info.0);
-        // _pause();
-
-        //println!("huh?");
-        for (ns, ninfo) in neighbors(&vec, p, info) {
-            let new_cost = cost.0 + vec[ns.0][ns.1];
-            //if seen.insert((ns, new_cost)) {
-            if seen.insert((ns, ninfo)) {
-                prev.insert((ns, ninfo), (p, info));
-                stack.push((Reverse(new_cost), ns, ninfo));
-            }
-        }
-        //println!("nvm");
-    }
-
-    unreachable!()
-}
-
-fn neighbors2(
+fn neighbors<const MIN: usize, const MAX: usize>(
     vec: &[Vec<usize>],
     idx: (usize, usize),
     (gdir, count): (Dir, usize),
@@ -179,69 +29,60 @@ fn neighbors2(
     let mx = vec[0].len() as isize;
     let y = idx.0 as isize;
     let x = idx.1 as isize;
+
+    let in_bounds = move |y: isize, x: isize| x >= 0 && x < mx && y >= 0 && y < my;
+    let not_reverse = move |new: Dir| {
+        !((new == Dir::Up && gdir == Dir::Down)
+            || (new == Dir::Down && gdir == Dir::Up)
+            || (new == Dir::Left && gdir == Dir::Right)
+            || (new == Dir::Right && gdir == Dir::Left))
+    };
+
+    let allowed_steps =
+        move |new: Dir| !(new == gdir && count == (MAX - 1) || new != gdir && count < (MIN - 1));
+
+    let get_dir = move |y: isize, x: isize| {
+        if y == idx.0 as isize {
+            if x > idx.1 as isize {
+                Dir::Right
+            } else {
+                Dir::Left
+            }
+        } else if y > idx.0 as isize {
+            Dir::Down
+        } else {
+            Dir::Up
+        }
+    };
+
     [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)]
         .into_iter()
-        .filter(move |(y, x)| *x >= 0 && *x < mx && *y >= 0 && *y < my)
-        .map(|(y, x)| (y as usize, x as usize))
-        .map(move |(y, x)| {
-            let dir = if y == idx.0 {
-                if x > idx.1 {
-                    Dir::Right
-                } else {
-                    Dir::Left
-                }
-            } else if x == idx.1 {
-                if y > idx.0 {
-                    Dir::Down
-                } else {
-                    Dir::Up
-                }
-            } else {
-                unreachable!();
-            };
-
-            (y, x, dir)
-        })
-        .filter(move |(_y, _x, dir)| {
-            !((*dir == Dir::Up && gdir == Dir::Down)
-                || (*dir == Dir::Down && gdir == Dir::Up)
-                || (*dir == Dir::Left && gdir == Dir::Right)
-                || (*dir == Dir::Right && gdir == Dir::Left)
-                || (*dir == gdir && count == 9))
-        })
-        .filter(move |(_y, _x, dir)| {
-            if count < 3 && gdir != *dir {
-                false
-            } else {
-                true
-            }
-        })
-        .map(move |(y, x, dir)| {
+        .filter_map(move |(y, x)| {
+            let dir = get_dir(y, x);
             let ncount = if dir == gdir { count + 1 } else { 0 };
-            ((y, x), (dir, ncount))
+            (in_bounds(y, x) && not_reverse(dir) && allowed_steps(dir))
+                .then(|| ((y as usize, x as usize), (dir, ncount)))
         })
 }
 
-fn task_two(input: &[String]) -> usize {
+fn least_heat_loss<const MIN: usize, const MAX: usize>(input: &[String]) -> usize {
     let vec = parse(input);
     let mut seen = HashSet::new();
     let start = (0, 0);
     seen.insert((start, (Dir::Right, 0)));
 
-    let mut stack = BinaryHeap::new();
-    use std::cmp::Reverse;
+    let end = (vec.len() - 1, vec[0].len() - 1);
 
+    let mut stack = BinaryHeap::new();
     stack.push((Reverse(0), start, (Dir::Right, 0)));
     stack.push((Reverse(0), start, (Dir::Down, 0)));
 
-    let end = (vec.len() - 1, vec[0].len() - 1);
-
     while let Some((cost, p, info)) = stack.pop() {
-        if p == end && info.1 >= 3 {
+        if p == end && info.1 >= (MIN - 1) {
             return cost.0;
         }
 
-        for (ns, ninfo) in neighbors2(&vec, p, info) {
+        for (ns, ninfo) in neighbors::<MIN, MAX>(&vec, p, info) {
             let new_cost = cost.0 + vec[ns.0][ns.1];
             if seen.insert((ns, ninfo)) {
                 stack.push((Reverse(new_cost), ns, ninfo));
@@ -250,6 +91,14 @@ fn task_two(input: &[String]) -> usize {
     }
 
     unreachable!()
+}
+
+fn task_one(input: &[String]) -> usize {
+    least_heat_loss::<1, 3>(input)
+}
+
+fn task_two(input: &[String]) -> usize {
+    least_heat_loss::<4, 10>(input)
 }
 
 fn main() {
