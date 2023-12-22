fe16b2e0470e15db9fe35a9f636586092b219448
Sivert Johansen <sivert@hotmail.com>
2023-12-22 14:37:47 +0100

    update(day21) ✨

--- 2023/day_21/main.rs
+++ 2023/day_21/main.rs
@@ -1,10 +1,11 @@
 use std::collections::*;
+type HashMap<K, V> = std::collections::HashMap<K, V, fxhash::FxBuildHasher>;
 
 type Map = HashMap<(isize, isize), char>;
 
 fn parse(input: &[String]) -> ((isize, isize), Map) {
     let mut start = (-1, -1);
-    let mut map = HashMap::new();
+    let mut map = HashMap::default();
     for (y, line) in input.iter().enumerate() {
         for (x, ch) in line.chars().enumerate() {
             if ch == 'S' {
@@ -27,75 +28,50 @@ fn neighbors(map: &Map, pos: (isize, isize)) -> impl Iterator<Item = (isize, isi
         .filter(|(y, x)| matches!(map.get(&(*y, *x)), Some(ch) if *ch == '.'))
 }
 
-fn bfs(map: &Map, start: (isize, isize), ss: isize) -> usize {
-    let mut ans = HashSet::new();
-    let mut seen = HashSet::new();
-    seen.insert(start);
+fn bfs(map: &Map, start: (isize, isize)) -> HashMap<(isize, isize), usize> {
+    let mut dist = HashMap::with_hasher(fxhash::FxBuildHasher::default());
+    dist.insert(start, 0);
 
     let mut vec = VecDeque::new();
-    vec.push_back((start, ss));
+    vec.push_back((0, start));
 
-    while let Some((pos, s)) = vec.pop_front() {
-        if s % 2 == 0 {
-            ans.insert(pos);
-        }
-        if s == 0 {
-            continue;
-        }
-
+    while let Some((cost, pos)) = vec.pop_front() {
         for ns in neighbors(map, pos) {
-            if seen.insert(ns) {
-                vec.push_back((ns, s - 1));
-            }
+            dist.entry(ns).or_insert_with(|| {
+                vec.push_back((cost + 1, ns));
+                cost + 1
+            });
         }
     }
 
-    ans.len()
+    dist
 }
 
 fn task_one(input: &[String]) -> usize {
     let (start, map) = parse(input);
-    bfs(&map, start, 64)
+    let dist = bfs(&map, start);
+    dist.into_values()
+        .filter(|n| *n <= 64 && n % 2 == 0)
+        .count()
 }
 
 fn task_two(input: &[String]) -> usize {
     let (start, map) = parse(input);
-    math_brr(&map, input.len(), start, 26501365)
-}
+    let dist = bfs(&map, start);
 
-fn math_brr(map: &Map, size: usize, (sr, sc): (isize, isize), steps: usize) -> usize {
-    let grid_width = (steps / size - 1) as usize;
-    let size = size as isize;
+    let even_corners = dist.values().filter(|v| **v % 2 == 0 && **v > 65).count();
+    let odd_corners = dist.values().filter(|v| **v % 2 == 1 && **v > 65).count();
 
-    let odd = (grid_width / 2 * 2 + 1).pow(2) as usize;
-    let even = ((grid_width + 1) / 2 * 2).pow(2) as usize;
+    let n = (26501365 - (input.len() / 2)) / input.len();
+    assert_eq!(n, 202300);
 
-    let odd_points = bfs(&map, (sr, sc), size * 2 + 1);
-    let even_points = bfs(&map, (sr, sc), size * 2);
+    let even = n * n;
+    let odd = (n + 1) * (n + 1);
 
-    let corner_t = bfs(&map, (size - 1, sc), size - 1);
-    let corner_r = bfs(&map, (sr, 0), size - 1);
-    let corner_b = bfs(&map, (0, sc), size - 1);
-    let corner_l = bfs(&map, (sr, size - 1), size - 1);
+    let odd_full = dist.values().filter(|v| **v % 2 == 1).count();
+    let even_full = dist.values().filter(|v| **v % 2 == 0).count();
 
-    let small_tr = bfs(&map, (size - 1, 0), size / 2 - 1);
-    let small_tl = bfs(&map, (size - 1, size - 1), size / 2 - 1);
-    let small_br = bfs(&map, (0, 0), size / 2 - 1);
-    let small_bl = bfs(&map, (0, size - 1), size / 2 - 1);
-
-    let large_tr = bfs(&map, (size - 1, 0), size * 3 / 2 - 1);
-    let large_tl = bfs(&map, (size - 1, size - 1), size * 3 / 2 - 1);
-    let large_br = bfs(&map, (0, 0), size * 3 / 2 - 1);
-    let large_bl = bfs(&map, (0, size - 1), size * 3 / 2 - 1);
-
-    (odd as usize) * odd_points
-        + even * even_points
-        + corner_t
-        + corner_r
-        + corner_b
-        + corner_l
-        + (grid_width + 1) * (small_tr + small_tl + small_br + small_bl)
-        + grid_width * (large_tr + large_tl + large_br + large_bl)
+    odd * odd_full + even * even_full - ((n + 1) * odd_corners) + (n * even_corners)
 }
 
 fn main() {
