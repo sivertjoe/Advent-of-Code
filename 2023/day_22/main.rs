da325b6db8ee7e27309010eebcfa441387f85470
Sivert Johansen <sivert@hotmail.com>
2023-12-22 13:09:58 +0100

    add(day22) 💩

--- 2023/day_22/main.rs
+++ 2023/day_22/main.rs
@@ -1,45 +1,25 @@
 use std::collections::*;
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Point {
     x: isize,
     y: isize,
     z: isize,
 }
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-struct Brick(Point, Point);
-impl Brick {
-    fn lowest_z(&self) -> isize {
-        if self.0.z < self.1.z {
-            self.0.z
-        } else {
-            self.1.z
-        }
-    }
-    fn get_ys(&self) -> (isize, isize) {
-        if self.0.y < self.1.y {
-            (self.0.y, self.1.y)
-        } else {
-            (self.1.y, self.0.y)
-        }
-    }
-    fn get_xs(&self) -> (isize, isize) {
-        if self.0.x < self.1.x {
-            (self.0.x, self.1.x)
-        } else {
-            (self.1.x, self.0.x)
-        }
-    }
-
-    fn overlaps(&self, p: &Brick) -> bool {
-        let (st, ed) = self.get_ys();
-        let (st2, ed2) = p.get_ys();
-        false
+use std::fmt;
+impl fmt::Debug for Brick {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}: [{:?} {:?}]", self.2 as u8 as char, self.0, self.1)
     }
 }
 
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct Brick(Point, Point, usize);
+
 fn parse(input: &[String]) -> Vec<Brick> {
+    let mut id: usize = b'A' as usize;
     let p = |s: Option<&str>| -> isize { s.unwrap().parse::<isize>().unwrap() };
 
     let parse = |s: &str| {
@@ -54,82 +34,112 @@ fn parse(input: &[String]) -> Vec<Brick> {
         .iter()
         .map(|line| {
             let (a, b) = line.split_once('~').unwrap();
-            Brick(parse(a), parse(b))
+            let b = Brick(parse(a), parse(b), id);
+            id += 1;
+            b
         })
         .collect::<Vec<_>>()
 }
 
-fn find_overlapping_bricks_above(brick: Brick, bricks: &[Brick]) -> Vec<Brick> {
-    let mut vec = Vec::new();
-
-    for b in bricks {
-        if brick == *b {
-            continue;
-        }
-        if b.lowest_z() >= brick.lowest_z() {
-            continue;
-        }
+fn is_solid(set: &HashSet<(isize, isize, isize)>, x: isize, y: isize, z: isize) -> bool {
+    if z == 0 {
+        true 
+    } else {
+        set.contains(&(x, y, z))
     }
-
-    vec
-}
-
-fn bricks_overlap(brick1: &Brick, brick2: &Brick) -> bool {
-    let a1 = brick1.0;
-    let b1 = brick1.1;
-    let a2 = brick2.0;
-    let b2 = brick2.1;
-
-    let denominator = (b1.x - a1.x) * (b2.y - a2.y) - (b1.y - a1.y) * (b2.x - a2.x);
-
-    if denominator == 0 {
-        // Lines are parallel or coincident
-        return false;
-    }
-
-    let t = ((a2.x - a1.x) * (b2.y - a2.y) - (a2.y - a1.y) * (b2.x - a2.x)) / denominator;
-    let s = ((a2.x - a1.x) * (b1.y - a1.y) - (a2.y - a1.y) * (b1.x - a1.x)) / denominator;
-
-    // Check if the values of t and s are within the valid range (0 to denominator)
-    t >= 0 && t <= denominator && s >= 0 && s <= denominator
-}
-
-fn collides_with_other(brick: &Brick, bricks: &[Brick]) -> bool {
-    for b in bricks {
-        if b == brick { continue; }
-        if bricks_overlap(brick, b) {
-            return true;
-        }
-    }
-    false
 }
 
 
-fn fall(bricks: &mut [Brick]) {
-    loop {
-        let mut change = false;
-        for i in 0..bricks.len() {
-            let cpy = bricks[i];
 
-            while cpy.lowest_z() > 0 && 
-        }
+fn fall(bricks: Vec<Brick>) -> (bool, Vec<Brick>) {
+    let mut change = false;
+    let mut set = HashSet::new();
 
-        if !change {
-            break;
+    for brick in &bricks {
+        for x in brick.0.x..=brick.1.x {
+            for y in brick.0.y..=brick.1.y {
+                set.insert((x, y, brick.1.z));
+            }
         }
     }
+
+    let mut new_bricks = Vec::new();
+
+    for brick in bricks {
+        let mut supp = false;
+        'outer: for x in brick.0.x..=brick.1.x {
+            for y in brick.0.y..=brick.1.y {
+                if is_solid(&set, x, y, brick.0.z - 1) {
+                    supp = true;
+                    break 'outer;
+                }
+            }
+        }
+
+        if !supp {
+            change = true;
+            let mut cpy = brick.clone();
+            cpy.0.z -= 1;
+            cpy.1.z -= 1;
+            new_bricks.push(cpy);
+        } else {
+            new_bricks.push(brick.clone());
+        }
+    }
+
+    (change, new_bricks)
 }
 
 fn task_one(input: &[String]) -> isize {
-    let bricks = parse(input);
+    let mut bricks = parse(input);
     let mut sum = 0;
 
-    for brick in &bricks {}
+    let mut change = true;
+    while change {
+        (change, bricks) = fall(bricks);
+    }
+
+    for i in 0..bricks.len() {
+        let mut tmp = bricks.clone();
+        tmp.remove(i);
+        if !fall(tmp).0 {
+            sum += 1;
+        }
+    }
     sum
 }
 
 fn task_two(input: &[String]) -> isize {
-    unimplemented!()
+    let mut bricks = parse(input);
+    let mut sum = 0;
+
+    let mut change = true;
+    while change {
+        (change, bricks) = fall(bricks);
+    }
+
+    for i in 0..bricks.len() {
+        let mut copy = bricks.clone();
+        copy.remove(i);
+        
+        let mut lchange = true;
+        while lchange {
+            (lchange, copy) = fall(copy);
+        }
+        let mut map = HashMap::new();
+        for cpy in copy {
+            map.insert(cpy.2, cpy.0.z);
+        }
+
+        for b in &bricks {
+            let Some(z0) = map.get(&b.2) else { continue; };
+            if b.0.z != *z0 {
+                sum += 1;
+            }
+        }
+    }
+    sum
+
 }
 
 fn main() {
