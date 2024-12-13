daf2c51359502021a8d5d1ceb8d22728c29e5d1e
Sivert Johansen <sivert.johansen@grade.com>
2024-12-13 13:35:07 +0100

    update(12): improve performance

--- 2024/day_12/src/main.rs
+++ 2024/day_12/src/main.rs
@@ -1,322 +1,103 @@
 use std::collections::*;
 
-fn neighbors(map: &[Vec<u8>], (y, x): (usize, usize)) -> impl Iterator<Item = (usize, usize)> {
+type P = (isize, isize);
+type HashSet<T> = fxhash::FxHashSet<T>;
+
+const DIRS: [(isize, isize); 4] = [
+    (1, 0),
+    (0, 1),
+    (0, -1),
+    (-1, 0),
+];
+
+fn neighbors<T>(map: &[Vec<T>], (y, x): (isize, isize)) -> impl Iterator<Item = (isize, isize)> {
     let my = map.len() as isize;
     let mx = map[0].len() as isize;
-    let x = x as isize;
-    let y = y as isize;
-    [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)]
-        .into_iter()
+    DIRS
+        .iter()
+        .map(move |dir| (y + dir.0, x + dir.1))
         .filter(move |(y, x)| *x >= 0 && *x < mx && *y >= 0 && *y < my)
-        .map(|(y, x)| (y as usize, x as usize))
 }
 
-fn find_area_and_perimiter(
-    (y, x): (usize, usize),
-    map: &[Vec<u8>],
-) -> (usize, usize, HashSet<(usize, usize)>) {
-    let start = (y, x);
-    let ch = map[y][x];
+fn bfs(pos: (isize, isize), map: &[Vec<char>]) -> (HashSet<(isize, isize)>, usize) {
+    let mut vec = VecDeque::new();
+    let mut seen = HashSet::default();
 
-    let mut vec = vec![start];
-    let mut seen = HashSet::new();
+    let plant = map[pos.0 as usize][pos.1 as usize];
+    let mut perimiter = 0;
 
-    let mut perim = 0;
+    vec.push_back(pos);
+    seen.insert(pos);
 
-    while let Some((ny, nx)) = vec.pop() {
-        if map[ny][nx] != ch {
-            continue;
-        }
-
-        if !seen.insert((ny, nx)) {
-            continue;
-        }
-
-        let ns = neighbors(map, (ny, nx))
-            .filter(|(_ny, _nx)| map[*_ny][*_nx] == ch)
-            .collect::<Vec<_>>();
-
-        perim += 4 - ns.len();
-
+    while  let Some(curr) = vec.pop_front() {
+        let ns = neighbors(map, curr).filter(|(y, x)| map[*y as usize][*x as usize] == plant).collect::<Vec<_>>();
+        perimiter += 4 - ns.len();
         for n in ns {
-            vec.push(n);
+            if seen.insert(n) {
+               vec.push_back(n);
+            }
         }
     }
 
-    (seen.len(), perim, seen)
+    (seen, perimiter)
+}
+
+fn count_sides(shape: &HashSet<(isize, isize)>) -> usize {
+    let mut sum = 0;
+    for dir in DIRS {
+        let sides = shape
+            .iter()
+            .map(|pos| (pos.0 + dir.0, pos.1 + dir.1))
+            .filter(|next| !shape.contains(next))
+            .collect::<HashSet<_>>();
+
+        let mut remove = HashSet::default();
+        for side in &sides {
+            let mut tmp = (side.0 + dir.1, side.1 + dir.0);
+            while sides.contains(&tmp) {
+                remove.insert(tmp);
+                tmp = (tmp.0 + dir.1, tmp.1 + dir.0);
+            }
+        }
+        sum += sides.len() - remove.len();
+    }
+
+    sum
+}
+
+fn solve<F>(input: &[String], mut f: F) -> usize
+    where F: FnMut(P, &[Vec<char>]) -> (usize, HashSet<P>)
+{
+    let map: Vec<Vec<_>> = input.iter().map(|line| line.chars().collect()).collect();
+
+    let mut sum = 0;
+    let mut seen = HashSet::default();
+    for y in 0..map.len() {
+        for x in 0..map[0].len() {
+            if seen.insert((y as isize, x as isize)) {
+                let (s, area) = f((y as isize, x as isize), &map);
+                sum += s;
+                seen.extend(area);
+            }
+        }
+    }
+
+    sum
 }
 
 fn task_one(input: &[String]) -> usize {
-    let map: Vec<Vec<_>> = input.iter().map(|line| line.bytes().collect()).collect();
-
-    let mut sum = 0;
-    let mut seen = HashSet::new();
-    for y in 0..map.len() {
-        for x in 0..map[0].len() {
-            if seen.insert((y, x)) {
-                let (a, p, s) = find_area_and_perimiter((y, x), &map);
-                sum += a * p;
-
-                for p in s {
-                    seen.insert(p);
-                }
-            }
-        }
-    }
-
-    sum
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Dir {
-    Left,
-    Right,
-    Up,
-    Down,
-}
-
-impl Dir {
-    fn rotate_wall(&self) -> Self {
-        match self {
-            Dir::Up => Dir::Left,
-            Dir::Left => Dir::Down,
-            Dir::Down => Dir::Right,
-            Dir::Right => Dir::Up,
-        }
-    }
-    fn rotate_air(&self) -> Self {
-        match self {
-            Dir::Up => Dir::Right,
-            Dir::Right => Dir::Down,
-            Dir::Down => Dir::Left,
-            Dir::Left => Dir::Up,
-        }
-    }
-}
-fn print_shape2(seen: &HashSet<P>) {
-    if seen.is_empty() {
-        return;
-    }
-    let max_y = seen.iter().map(|p| p.0).max().unwrap();
-    let max_x = seen.iter().map(|p| p.1).max().unwrap();
-
-    let min_y = seen.iter().map(|p| p.0).min().unwrap();
-    let min_x = seen.iter().map(|p| p.1).min().unwrap();
-
-    for y in min_y - 1..=max_y + 1 {
-        for x in min_x - 1..=max_x + 1 {
-            if seen.contains(&(y, x)) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!();
-    }
-    println!();
-}
-
-fn print_shape(seen: &HashSet<P>, pos: P, dir: Dir) {
-    let max_y = seen.iter().map(|p| p.0).max().unwrap();
-    let max_x = seen.iter().map(|p| p.1).max().unwrap();
-
-    let min_y = seen.iter().map(|p| p.0).min().unwrap();
-    let min_x = seen.iter().map(|p| p.1).min().unwrap();
-
-    let hug = hug_pos(pos, dir);
-
-    let ch = match dir {
-        Dir::Up => '^',
-        Dir::Down => 'v',
-        Dir::Right => '>',
-        Dir::Left => '<',
-    };
-
-    for y in min_y - 1..=max_y + 1 {
-        for x in min_x - 1..=max_x + 1 {
-            if (y, x) == hug {
-                print!("O");
-            } else if seen.contains(&(y, x)) {
-                print!("#");
-            } else if (y, x) == pos {
-                print!("{ch}");
-            } else {
-                print!(".");
-            }
-        }
-        println!();
-    }
-    println!();
-}
-type P = (isize, isize);
-
-fn hug_pos(pos: P, dir: Dir) -> P {
-    match dir {
-        Dir::Up => (pos.0, pos.1 + 1),
-        Dir::Down => (pos.0, pos.1 - 1),
-        Dir::Right => (pos.0 + 1, pos.1),
-        Dir::Left => (pos.0 - 1, pos.1),
-    }
-}
-
-fn try_move(pos: P, dir: Dir, seen: &HashSet<P>) -> Result<Result<P, (P, Dir)>, Dir> {
-    let next = match dir {
-        Dir::Left => (pos.0, pos.1 - 1),
-        Dir::Right => (pos.0, pos.1 + 1),
-        Dir::Up => (pos.0 - 1, pos.1),
-        Dir::Down => (pos.0 + 1, pos.1),
-    };
-
-    if seen.contains(&next) {
-        return Err(dir.rotate_wall());
-    }
-
-    let hug = hug_pos(next, dir);
-    if seen.contains(&hug) {
-        return Ok(Ok(next));
-    }
-
-    let dir = dir.rotate_air();
-    let next = match dir {
-        Dir::Left => (next.0, next.1 - 1),
-        Dir::Right => (next.0, next.1 + 1),
-        Dir::Up => (next.0 - 1, next.1),
-        Dir::Down => (next.0 + 1, next.1),
-    };
-    Ok(Err((next, dir)))
-}
-
-fn calculate_sides(seen: &HashSet<(usize, usize)>) -> usize {
-    if seen.is_empty() {
-        return 0;
-    }
-    let seen = seen
-        .iter()
-        .map(|(y, x)| (*y as isize, *x as isize))
-        .collect::<HashSet<_>>();
-
-    let start = seen.iter().min().unwrap();
-    let start = (start.0 - 1, start.1);
-    let mut dir = Dir::Down;
-
-    dir = try_move(start, dir, &seen).unwrap_err();
-    let odir = dir;
-    let mut pos = start;
-
-    let mut sum = 0;
-    loop {
-        match try_move(pos, dir, &seen) {
-            Ok(Ok(next)) => pos = next,
-            Ok(Err((next, ndir))) => {
-                sum += 1;
-                pos = next;
-                dir = ndir;
-            }
-            Err(ndir) => {
-                sum += 1;
-                dir = ndir;
-            }
-        };
-        if pos == start && dir == odir {
-            break;
-        }
-    }
-
-    sum
-}
-
-fn pause() {
-    let mut _buf = String::new();
-    let _ = std::io::stdin().read_line(&mut _buf);
-}
-
-type U = (usize, usize);
-
-fn flood((y, x): U, map: &[Vec<u8>], shape: &HashSet<U>) -> HashSet<(usize, usize)> {
-    let start = (y, x);
-
-    let mut vec = vec![start];
-    let mut seen = HashSet::new();
-
-    while let Some((ny, nx)) = vec.pop() {
-        if shape.contains(&(ny, nx)){
-            continue;
-        }
-
-        if !seen.insert((ny, nx)) {
-            continue;
-        }
-
-        let ns = neighbors(map, (ny, nx))
-            .filter(|(_ny, _nx)| !shape.contains(&(*_ny, *_nx)))
-            .collect::<Vec<_>>();
-
-        for n in ns {
-            vec.push(n);
-        }
-    }
-
-    seen
-}
-
-fn find_insides(shape: &HashSet<U>, map: &[Vec<u8>], _ingore: u8) -> Vec<HashSet<U>> {
-    let max_y = shape.iter().map(|p| p.0).max().unwrap();
-    let max_x = shape.iter().map(|p| p.1).max().unwrap();
-
-    let min_y = shape.iter().map(|p| p.0).min().unwrap();
-    let min_x = shape.iter().map(|p| p.1).min().unwrap();
-    let mut inside = Vec::new();
-
-    let mut seen = shape.clone();
-    for y in min_y..=max_y {
-        for x in min_x..=max_x {
-            if !seen.contains(&(y, x)) {
-                let s = flood((y as usize, x as usize), map, shape);
-
-                let goes_to_edge = s
-                    .iter()
-                    .any(|&(y, x)| y == 0 || x == 0 || y == map.len() - 1 || x == map[0].len() - 1);
-                if !goes_to_edge {
-                    for p in s.iter() {
-                        seen.insert((p.0 as _, p.1 as _));
-                    }
-                    inside.push(s);
-                }
-            }
-        }
-    }
-
-    inside
+    solve(input, |pos, map| {
+        let (area, perimiter) = bfs(pos, &map);
+        (area.len() * perimiter, area)
+    })
 }
 
 fn task_two(input: &[String]) -> usize {
-    let map: Vec<Vec<_>> = input.iter().map(|line| line.bytes().collect()).collect();
-
-    let mut sum = 0;
-    let mut seen = HashSet::new();
-    for y in 0..map.len() {
-        for x in 0..map[0].len() {
-            if map[y][x] == b'.' {
-                continue;
-            }
-            if seen.insert((y, x)) {
-                let (a, _p, s) = find_area_and_perimiter((y, x), &map);
-                let mut sides = calculate_sides(&s);
-
-                let inside = find_insides(&s, &map, map[y][x]);
-                for s in inside {
-                    sides += calculate_sides(&s);
-                }
-
-                for p in s {
-                    seen.insert(p);
-                }
-                sum += a * sides;
-            }
-        }
-    }
-
-    sum
+    solve(input, |pos, map|{
+        let (area, _) = bfs(pos, &map);
+        let sides = count_sides(&area);
+        (area.len() * sides, area)
+    })
 }
 
 fn main() {
@@ -373,163 +154,4 @@ fn get_input_file() -> String {
     std::env::args()
         .nth(1)
         .unwrap_or_else(|| "input".to_string())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn input_1() {
-        let input = [
-            "AAAA".to_string(),
-            "BBCD".to_string(),
-            "BBCC".to_string(),
-            "EEEC".to_string(),
-        ];
-
-        assert_eq!(task_two(&input), 80);
-    }
-    #[test]
-    fn input_2() {
-        let input = [
-            "OOOOO".to_string(),
-            "OXOXO".to_string(),
-            "OOOOO".to_string(),
-            "OXOXO".to_string(),
-            "OOOOO".to_string(),
-        ];
-
-        assert_eq!(task_two(&input), 436);
-    }
-    #[test]
-    fn input_3() {
-        let input = [
-            "RRRRIICCFF".to_string(),
-            "RRRRIICCCF".to_string(),
-            "VVRRRCCFFF".to_string(),
-            "VVRCCCJFFF".to_string(),
-            "VVVVCJJCFE".to_string(),
-            "VVIVCCJJEE".to_string(),
-            "VVIIICJJEE".to_string(),
-            "MIIIIIJJEE".to_string(),
-            "MIIISIJEEE".to_string(),
-            "MMMISSJEEE".to_string(),
-        ];
-
-        assert_eq!(task_two(&input), 1206);
-    }
-    #[test]
-    fn input_4() {
-        let input = [
-            "EEEEE".to_string(),
-            "EXXXX".to_string(),
-            "EEEEE".to_string(),
-            "EXXXX".to_string(),
-            "EEEEE".to_string(),
-        ];
-
-        assert_eq!(task_two(&input), 236);
-    }
-    #[test]
-    fn input_5() {
-        let input = [
-            "AAAAAA".to_string(),
-            "AAABBA".to_string(),
-            "AAABBA".to_string(),
-            "ABBAAA".to_string(),
-            "ABBAAA".to_string(),
-            "AAAAAA".to_string(),
-        ];
-
-        assert_eq!(task_two(&input), 368);
-    }
-    #[test]
-    fn input_6() {
-        let input = [
-            "AAAAAA".to_string(),
-            "AAABBA".to_string(),
-            "AAABBA".to_string(),
-            "ABBAAA".to_string(),
-            "ABBAAA".to_string(),
-            "AAAAAA".to_string(),
-        ];
-
-        assert_eq!(task_two(&input), 368);
-    }
-    #[test]
-    fn input_7() {
-        let input = [
-            "OOOOO".to_string(),
-            "OXOXO".to_string(),
-            "OXXXO".to_string(),
-        ];
-
-        assert_eq!(task_two(&input), 160);
-    }
-    #[test]
-    fn input_8() {
-        let input = [
-            "CCAAA".to_string(),
-            "CCAAA".to_string(),
-            "AABBA".to_string(),
-            "AAAAA".to_string(),
-        ];
-
-        assert_eq!(task_two(&input), 164);
-    }
-
-    #[test]
-    fn input_9() {
-        let input = [
-            "AAAAAAAA".to_string(),
-            "AACBBDDA".to_string(),
-            "AACBBAAA".to_string(),
-            "ABBAAAAA".to_string(),
-            "ABBADDDA".to_string(),
-            "AAAADADA".to_string(),
-            "AAAAAAAA".to_string(),
-        ];
-
-        assert_eq!(task_two(&input), 946);
-    }
-    #[test]
-    fn input_10() {
-        let input = [
-            "...................".to_string(),
-            ".....#...#.#####...".to_string(),
-            "....##...#######.#.".to_string(),
-            "....##############.".to_string(),
-            ".################..".to_string(),
-            "..###############..".to_string(),
-            ".....#########.....".to_string(),
-            "......######.......".to_string(),
-            ".......#####.......".to_string(),
-            ".......######......".to_string(),
-            "........#####......".to_string(),
-            "......#####........".to_string(),
-            "...................".to_string(),
-        ];
-
-        assert_eq!(task_two(&input), 98 * 42);
-    }
-
-    #[test]
-    fn input_11() {
-        let input = [
-            ".............I...I.IIIII...".to_string(),
-            "............II...IIIIIII.I.".to_string(),
-            ".....I......IIIIIIIIIIIIII.".to_string(),
-            ".....I...IIIIIIIIIIIIIIII..".to_string(),
-            "...IIIIII.IIIIIIIIIIIIIII..".to_string(),
-            "...IIIIIII...IIIIIIIII.....".to_string(),
-            "..IIIIIIII....IIIIII.......".to_string(),
-            "..I.IIIII......IIIII.......".to_string(),
-            "....IIIII......IIIIII......".to_string(),
-            "....II..........IIIII......".to_string(),
-            "..............IIIII........".to_string()
-        ];
-
-        assert_eq!(task_two(&input), 98 * 42 + 36 * 20);
-    }
 }
