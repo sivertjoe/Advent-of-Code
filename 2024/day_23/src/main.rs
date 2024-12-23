655e8e5cf24c4b508235edcdbc85303c959843d9
sivertjoe <sivert-joh@hotmail.com>
2024-12-23 11:25:11 +0100

    update(23): pretty

--- 2024/day_23/src/main.rs
+++ 2024/day_23/src/main.rs
@@ -1,82 +1,20 @@
-use petgraph::prelude::*;
 use std::collections::*;
 
-fn task_one(input: &[String]) -> usize {
+fn parse(input: &[String]) -> HashMap<String, HashSet<String>> {
     let mut map: HashMap<String, HashSet<String>> = HashMap::new();
-    let mut edges = Vec::new();
 
     for line in input {
         let (a, b) = line.split_once('-').unwrap();
         map.entry(a.to_string()).or_default().insert(b.to_string());
         map.entry(b.to_string()).or_default().insert(a.to_string());
-
-        edges.push((a.to_string(), b.to_string()));
     }
-
-    let mut triangles = HashSet::new();
-    for (a, b) in &edges {
-        let common = map[a].intersection(&map[b]);
-        for w in common {
-            let mut arr = [a, b, w];
-            arr.sort();
-            triangles.insert(arr);
-        }
-    }
-
-    triangles
-        .into_iter()
-        .filter(|arr| arr.iter().any(|e| e.starts_with("t")))
-        .count()
-}
-
-fn find_connected_neighbors(
-    graph: &Graph<String, (), Undirected>,
-    index: NodeIndex,
-) -> HashSet<NodeIndex> {
-    // let neighbors: HashSet<NodeIndex<Ix>> = graph.neighbors(index).collect();
-    let neighbors: HashSet<NodeIndex> = graph
-        .neighbors_directed(index, Direction::Incoming)
-        .collect();
-
-    // Include the node itself
-    let mut connected_neighbors = neighbors.clone();
-    connected_neighbors.insert(index);
-
-    // Create a new set for the result
-    let mut result = HashSet::new();
-
-    for &node in &connected_neighbors {
-        // Get the neighbors of the current node
-        let node_neighbors: HashSet<NodeIndex> = graph
-            .neighbors_directed(node, Direction::Incoming)
-            .collect();
-
-        // Check if all other nodes in the group are in this node's neighbors
-        if connected_neighbors.is_subset(&node_neighbors) {
-            result.insert(node);
-        }
-    }
-
-    result
-}
-
-fn check_all(set: &[String], vec: &[(String, HashSet<String>)]) -> bool {
-    set.iter().all(|elem| {
-        vec.iter()
-            .filter(|(name, set)| elem != name)
-            .all(|(name, set)| set.contains(elem))
-    })
+    map
 }
 
 fn check_all_connected(
     mut set: Vec<String>,
     mut vec: Vec<(String, HashSet<String>)>,
 ) -> Vec<String> {
-    // First check if all are connected
-    if check_all(&set, &vec) {
-        return set;
-    }
-
     let clone = set.clone();
 
     for to_remove in clone {
@@ -98,67 +36,67 @@ fn check_all_connected(
         vec.push(temp);
     }
 
-    return Vec::new();
+    Vec::new()
+}
+
+fn check_all(set: &[String], vec: &[(String, HashSet<String>)]) -> bool {
+    set.iter().all(|elem| {
+        vec.iter()
+            .filter(|(name, _set)| elem != name)
+            .all(|(_name, set)| set.contains(elem))
+    })
+}
+
+fn find_connected_group(
+    k: &String,
+    v: &HashSet<String>,
+    map: &HashMap<String, HashSet<String>>,
+) -> Vec<String> {
+    let all = v.iter().cloned().collect::<Vec<_>>();
+
+    let mut vec = Vec::new();
+    for n in &all {
+        let connected = map[n].clone();
+        vec.push((n.clone(), connected));
+    }
+
+    let mut conn = check_all_connected(all, vec);
+    conn.push(k.clone());
+
+    conn
+}
+
+fn task_one(input: &[String]) -> usize {
+    let map = parse(input);
+
+    let mut triangles = HashSet::new();
+    for line in input {
+        let (a, b) = line.split_once('-').unwrap();
+        let common = map[a].intersection(&map[b]);
+        for w in common {
+            let mut arr = [a, b, w];
+            arr.sort();
+            triangles.insert(arr);
+        }
+    }
+
+    triangles
+        .into_iter()
+        .filter(|arr| arr.iter().any(|e| e.starts_with("t")))
+        .count()
 }
 
 fn task_two(input: &[String]) -> String {
-    let mut graph = Graph::new_undirected();
-    let mut indexes = HashMap::new();
-    let mut indexes_rev = HashMap::new();
+    let map = parse(input);
 
-    for line in input {
-        let (a, b) = line.split_once('-').unwrap();
-        let a = a.to_string();
-        let b = b.to_string();
+    let mut largest = map
+        .iter()
+        .map(|(k, v)| find_connected_group(k, v, &map))
+        .max_by_key(|v| v.len())
+        .unwrap();
 
-        if !indexes.contains_key(&a) {
-            let ao = graph.add_node(a.to_string());
-            indexes.insert(a.clone(), ao);
-            indexes_rev.insert(ao, a.clone());
-        }
-        if !indexes.contains_key(&b) {
-            let bo = graph.add_node(b.to_string());
-            indexes.insert(b.clone(), bo);
-            indexes_rev.insert(bo, b.clone());
-        }
-
-        let aa = indexes.get(&a).unwrap();
-        let bb = indexes.get(&b).unwrap();
-        graph.add_edge(*aa, *bb, ());
-    }
-
-    let mut largest = Vec::new();
-    for index in indexes.values() {
-        let connected: Vec<NodeIndex> = graph
-            .neighbors_directed(*index, Direction::Incoming)
-            .collect();
-
-        let all = connected
-            .iter()
-            .map(|i| indexes_rev[i].clone())
-            .collect::<Vec<_>>();
-
-        let mut vec = Vec::new();
-        for n in connected {
-            let connected: Vec<NodeIndex> =
-                graph.neighbors_directed(n, Direction::Incoming).collect();
-            let all = connected
-                .iter()
-                .map(|i| indexes_rev[i].clone())
-                .collect::<HashSet<_>>();
-            vec.push((indexes_rev[&n].clone(), all));
-        }
-
-        let mut conn = check_all_connected(all, vec);
-        conn.push(indexes_rev[index].clone());
-
-        if conn.len() > largest.len() {
-            largest = conn;
-        }
-    }
     largest.sort();
-    let password = largest.join(",");
-    password
+    largest.join(",")
 }
 
 fn main() {
