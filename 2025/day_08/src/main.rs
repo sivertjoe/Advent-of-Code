500a62b0b02f92bec82a1322a8487480d157a151
Sivert Johansen <sivert.johansen@grade.com>
2025-12-08 09:23:54 +0100

    add: day 8
    note: need to translate

--- 2025/day_08/src/main.rs
+++ 2025/day_08/src/main.rs
@@ -1,6 +1,6 @@
-use std::collections::*;
+use std::{cmp::Reverse, collections::*};
 
-#[derive(Eq, PartialEq, Hash, Clone)]
+#[derive(Eq, PartialEq, Hash, Clone, Debug, Copy)]
 struct Point {
     x: usize,
     y: usize,
@@ -13,10 +13,6 @@ impl Point {
             + (self.y as isize - other.y as isize).pow(2)
             + (self.z as isize - other.z as isize).pow(2)) as f32;
         inner.sqrt()
-    }
-
-    fn print(&self) {
-        println!("X={}, Y={}, Z={}", self.x, self.y, self.z);
     }
 }
 
@@ -41,56 +37,124 @@ fn task_one(input: &[String]) -> usize {
 
     for i in 0..points.len() {
         for j in i + 1..points.len() {
+            if i == j {
+                continue;
+            }
             let dist = points[i].dist(&points[j]);
             dists.insert((i, j), dist);
         }
     }
 
+    let mut dists2 = dists.into_iter().collect::<Vec<_>>();
+    dists2.sort_by(|a, b| a.1.total_cmp(&b.1));
+
     let mut circuts: Vec<HashSet<Point>> = Vec::new();
 
-    for _ in 0..10 {
-        let ((a, b), _dist) = dists.iter().min_by(|a, b| a.1.total_cmp(b.1)).unwrap();
+    for ((a, b), _dist) in dists2.into_iter().take(1000) {
+        let ap = points[a].clone();
+        let bp = points[b].clone();
 
-        let ap = points[*a].clone();
-        let bp = points[*b].clone();
-
-        ap.print();
-        bp.print();
-
-        let mut found = false;
-        for c in &mut circuts {
+        let mut found_a: Option<usize> = None;
+        let mut found_b: Option<usize> = None;
+        for (i, c) in circuts.iter_mut().enumerate() {
             if c.contains(&ap) {
-                c.insert(bp.clone());
-                found = true;
-            }
-            if c.contains(&bp) {
-                c.insert(ap.clone());
-                found = true;
+                found_a = Some(i);
+            } else if c.contains(&bp) {
+                found_b = Some(i);
             }
         }
 
-        if !found {
-            let mut set = HashSet::new();
-            set.insert(ap);
-            set.insert(bp);
-            circuts.push(set);
+        match (found_a, found_b) {
+            (Some(i), Some(j)) => {
+                let (max, min) = (std::cmp::max(i, j), std::cmp::min(i, j));
+                let set1 = circuts.swap_remove(max);
+                let set2 = circuts.swap_remove(min);
+
+                let union = set1.union(&set2).cloned().collect::<HashSet<Point>>();
+                circuts.push(union);
+            }
+            (Some(i), None) => {
+                circuts[i].insert(bp);
+            }
+            (None, Some(j)) => {
+                circuts[j].insert(ap);
+            }
+            (None, None) => {
+                let mut new_circut = HashSet::new();
+                new_circut.insert(ap);
+                new_circut.insert(bp);
+                circuts.push(new_circut);
+            }
         }
-        dists.remove(&(*a, *b));
-        for c in &circuts {
-            print!("{} ", c.len());
-        }
-        println!();
     }
 
-    for c in circuts {
-        println!("{}", c.len());
-    }
-
-    return 0;
+    circuts.sort_by_key(|set| Reverse(set.len()));
+    circuts.into_iter().map(|set| set.len()).take(3).product()
 }
 
 fn task_two(input: &[String]) -> usize {
-    unimplemented!()
+    let points = parse(input);
+
+    let mut dists = HashMap::new();
+
+    for i in 0..points.len() {
+        for j in i + 1..points.len() {
+            if i == j {
+                continue;
+            }
+            let dist = points[i].dist(&points[j]);
+            dists.insert((i, j), dist);
+        }
+    }
+
+    let mut dists2 = dists.into_iter().collect::<Vec<_>>();
+    dists2.sort_by(|a, b| a.1.total_cmp(&b.1));
+
+    let mut circuts: Vec<HashSet<Point>> = Vec::new();
+
+    for ((a, b), _dist) in dists2 {
+        let ap = points[a].clone();
+        let bp = points[b].clone();
+
+        let mut found_a: Option<usize> = None;
+        let mut found_b: Option<usize> = None;
+        for (i, c) in circuts.iter_mut().enumerate() {
+            if c.contains(&ap) {
+                found_a = Some(i);
+            } else if c.contains(&bp) {
+                found_b = Some(i);
+            }
+        }
+
+        match (found_a, found_b) {
+            (Some(i), Some(j)) => {
+                let (max, min) = (std::cmp::max(i, j), std::cmp::min(i, j));
+                let set1 = circuts.swap_remove(max);
+                let set2 = circuts.swap_remove(min);
+
+                let union = set1.union(&set2).cloned().collect::<HashSet<Point>>();
+                circuts.push(union);
+            }
+            (Some(i), None) => {
+                circuts[i].insert(bp);
+            }
+            (None, Some(j)) => {
+                circuts[j].insert(ap);
+            }
+            (None, None) => {
+                let mut new_circut = HashSet::new();
+                new_circut.insert(ap);
+                new_circut.insert(bp);
+                circuts.push(new_circut);
+            }
+        }
+
+        if circuts.len() == 1 && circuts.iter().next().unwrap().len() == points.len() {
+            return ap.x * bp.x;
+        }
+    }
+
+    return 0;
 }
 
 fn main() {
